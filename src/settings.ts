2856dfba10da546de79f95edb3b369bf1981ef4e
kamite0611 <kamiteakira@gmail.com>
2025-04-28 23:53:25 +0900

    save

--- src/settings.ts
+++ src/settings.ts
@@ -3,8 +3,9 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
-export const GOOGLE_SERVICE_ACCOUNT_EMAIL = process.env.SHEET_ID;
-export const GOOGLE_PRIVATE_KEY = process.env.SHEET_NAME;
+export const GOOGLE_SERVICE_ACCOUNT_EMAIL =
+  process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL;
+export const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY;
 
 if (!DISCORD_TOKEN || !GOOGLE_SERVICE_ACCOUNT_EMAIL || !GOOGLE_PRIVATE_KEY) {
   throw new Error("環境変数が設定されていません。");
